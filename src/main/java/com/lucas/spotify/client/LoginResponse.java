86b2880183525f6fac499d48e7081c6809383b63
Lucas <lucas.desenvolvedor@hotmail.com>
2023-12-13 16:27:30 -0300

    explorer api spotify

---
+++ src/main/java/com/lucas/spotify/client/LoginResponse.java
@@ -0,0 +1,29 @@
+package com.lucas.spotify.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public LoginResponse() {
+
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    
+    
+    
+}
--- src/main/java/com/lucas/spotify/controller/AlbumController.java
+++ src/main/java/com/lucas/spotify/controller/AlbumController.java
@@ -5,13 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucas.spotify.client.AuthSpotifyClient;
+import com.lucas.spotify.client.LoginRequest;
+
 @RestController
 @RequestMapping("/spotify/api")
 public class AlbumController {
 
+    private final AuthSpotifyClient authSpotifyClient;
+
+    public AlbumController(AuthSpotifyClient authSpotifyClient) {
+        this.authSpotifyClient = authSpotifyClient;
+    }
+
     @GetMapping("/albums")
     public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok().build();
+        var request = new LoginRequest(
+            "client_credentials",
+            "6973b09441f44ae3accc88af03f40a36",
+            "7aa17e0d85da483b9308893059e1d832"
+        );
+
+        return ResponseEntity.ok(authSpotifyClient.login(request).getAccessToken());
     }
     
 }
